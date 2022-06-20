a820509ee879362bd020f8f2349b8652b62365fb
Nicole <nicole.jayne.bergen@gmail.com>
2022-06-20 16:29:27 -0600

    June 20, take 2

--- statscan_languages_2016.r
+++ statscan_languages_2016.r
@@ -1,4 +1,10 @@
 library(tidyverse)
 languages_df <- read_csv("/Users/nicol/R_data/Indigenous_Languages.csv") # reassign path from the defaul /Users/nicol/...csv
+
 view(languages_df)
+
+ggplot(data = languages_df) +
+    geom_point(mapping = aes(x = Male, y = Languages, color = "red")) +
+    geom_point(mapping = aes(x = Female, y = Languages, color = "purple"))
+# to do: add column for language families to group by
 
