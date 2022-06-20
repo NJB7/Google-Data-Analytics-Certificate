bd1f0524b756334eb7363c8fcd0fde57fd94c202
Nicole <nicole.jayne.bergen@gmail.com>
2022-06-20 16:40:11 -0600

    June 20 last one today, I hope

--- statscan_languages_2016.r
+++ statscan_languages_2016.r
@@ -7,4 +7,4 @@ ggplot(data = languages_df) +
     geom_point(mapping = aes(x = Male, y = Languages, color = "red")) +
     geom_point(mapping = aes(x = Female, y = Languages, color = "purple"))
 # to do: add column for language families to group by
-
+# note: explore other visualizations, though I'm not sure this is the best dataset for that because there's no change over time # nolint
