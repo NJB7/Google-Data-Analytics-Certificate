8de698fd34bea2b0e7ce1aedd7fe2c917b6b7f87
Nicole <nicole.jayne.bergen@gmail.com>
2022-06-28 13:18:45 -0600

    June 28

--- Stats Canada - Languages - Mother Tongue - Single Response.sql
+++ Stats Canada - Languages - Mother Tongue - Single Response.sql
@@ -4,9 +4,19 @@ USE stats_canada_languages_2016;
 
 -- Used BigQuery to upload the admitedly small amount of data and ran these queries (results in github csvs):
 
-SELECT
-    *
+SELECT *
 FROM
     `reading-log-354623.data_set.Indigenous languages 1`
-WHERE
-    Total > 5000
+WHERE Total > 5000
+ORDER BY Total DESC;
+
+SELECT *
+FROM
+    `reading-log-354623.data_set.Indigenous languages 1`
+WHERE Total < 100;
+
+#because at first glance, and this was confirmed, there are more female speakers of most languages
+SELECT *
+FROM
+    `reading-log-354623.data_set.Indigenous languages 1`
+WHERE Male > Famle;
