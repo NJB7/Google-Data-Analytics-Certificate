3984373ddb717a60965f913a627bc11c49c5d450
Nicole <nicole.jayne.bergen@gmail.com>
2022-06-27 18:10:02 -0600

    Query 1

--- Stats Canada - Languages - Mother Tongue - Single Response.sql
+++ Stats Canada - Languages - Mother Tongue - Single Response.sql
@@ -2,4 +2,11 @@ CREATE DATABASE stats_canada_languages_2016;
 
 USE stats_canada_languages_2016;
 
--- Used BigQuery to upload the admitedly small amount of data and ran these queries:
+-- Used BigQuery to upload the admitedly small amount of data and ran these queries (results in github csvs):
+
+SELECT
+    *
+FROM
+    `reading-log-354623.data_set.Indigenous languages 1`
+WHERE
+    Total > 5000
